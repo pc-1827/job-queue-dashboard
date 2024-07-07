ad99fb48eb1f55cb22af8bd4ce55e4d31f96b2a4
pc-1827 <problemcreator1827@gmail.com>
2024-07-07 21:43:01 +0530

    dasbhoard template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+        <ul>
+          <li>Waiting</li>
+          <li>Active</li>
+          <li>Completed</li>
+          <li>Failed</li>
+          <li>Delayed</li>
+        </ul>
+      </nav>
+      <div className="job-details-box">
+        <h2>Burger#10</h2>
+        <div className="job-details-content">
+          <div className="job-meta">
+            <p><strong>Added at:</strong> 6:20:22 PM</p>
+            <p><strong>Process started at:</strong> 6:20:26 PM</p>
+            <p><strong>Failed at:</strong> 6:20:34 PM</p>
+          </div>
+          <div className="job-data-section">
+            <div className="job-details-navbar">
+              <ul>
+                <li>Data</li>
+                <li>Options</li>
+              </ul>
+            </div>
+            <pre className="job-data">
+              {`
+              {
+                "jobData": {
+                  "bun": "🍔",
+                  "cheese": "🧀",
+                  "toppings": [
+                    "🥬",
+                    "🍅",
+                    "🥒",
+                    "🌶️"
+                  ]
+                },
+                "returnValue": null
+              }
+              `}
+            </pre>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
